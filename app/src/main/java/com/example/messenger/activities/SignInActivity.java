02250075d4d4dadda35709a77ee77ead1c5e7336
DavSar06 <mr.davitsargsyan06@gmail.com>
2022-11-03 14:12:28 +0400

    Added binding, created switch from signIn to signUp and backwards

--- app/src/main/java/com/example/messenger/activities/SignInActivity.java
+++ app/src/main/java/com/example/messenger/activities/SignInActivity.java
@@ -2,15 +2,28 @@ package com.example.messenger.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.messenger.R;
+import com.example.messenger.databinding.ActivitySignInBinding;
 
 public class SignInActivity extends AppCompatActivity {
+
+    private ActivitySignInBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setListeners();
+    }
+
+    private void setListeners(){
+        binding.textCreateNewAccount.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(),SignUpActivity.class)));
     }
 }
--- app/src/main/java/com/example/messenger/activities/SignUpActivity.java
+++ app/src/main/java/com/example/messenger/activities/SignUpActivity.java
@@ -3,14 +3,25 @@ package com.example.messenger.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.messenger.R;
+import com.example.messenger.databinding.ActivitySignUpBinding;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private ActivitySignUpBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setListeners();
+    }
+
+    private void setListeners(){
+        binding.textSignIn.setOnClickListener(v -> onBackPressed());
     }
 }
